a8b7d1d187429c43ad69388c8e197d7b65e1efde
lordpdc <lordpdc@hotmail.com>
2016-10-01 18:25:35 -0500

    entidades de compra

--- src/business/entities/detailsPurchase.java
+++ src/business/entities/detailsPurchase.java
@@ -4,4 +4,49 @@ package business.entities;
  * Created by raoman on 01/10/2016.
  */
 public class detailsPurchase {
+    private int id;
+    private int idInvoicePurchase;
+    private int idConsumable;
+    private double priceConsumable;
+    private double quantityConsumable;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdInvoicePurchase() {
+        return idInvoicePurchase;
+    }
+
+    public void setIdInvoicePurchase(int idInvoicePurchase) {
+        this.idInvoicePurchase = idInvoicePurchase;
+    }
+
+    public int getIdConsumable() {
+        return idConsumable;
+    }
+
+    public void setIdConsumable(int idConsumable) {
+        this.idConsumable = idConsumable;
+    }
+
+    public double getPriceConsumable() {
+        return priceConsumable;
+    }
+
+    public void setPriceConsumable(double priceConsumable) {
+        this.priceConsumable = priceConsumable;
+    }
+
+    public double getQuantityConsumable() {
+        return quantityConsumable;
+    }
+
+    public void setQuantityConsumable(double quantityConsumable) {
+        this.quantityConsumable = quantityConsumable;
+    }
 }
--- src/business/entities/invoicePurchase.java
+++ src/business/entities/invoicePurchase.java
@@ -1,7 +1,45 @@
 package business.entities;
 
+import java.sql.Date;
+
 /**
  * Created by raoman on 01/10/2016.
  */
 public class invoicePurchase {
+    private int id;
+    private int idSupplier;
+    private Date date;
+    private double totalPrice;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdSupplier() {
+        return idSupplier;
+    }
+
+    public void setIdSupplier(int idSupplier) {
+        this.idSupplier = idSupplier;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
