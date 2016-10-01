04ecf02f384a53ded95e0a05575aca8ebc959bc4
lordpdc <lordpdc@hotmail.com>
2016-09-30 19:36:33 -0500

    customer

---
+++ src/business/utilities/NullElements.java
@@ -0,0 +1,11 @@
+package business.utilities;
+
+/**
+ * Created by raoman on 21/09/2016.
+ */
+public class NullElements {
+    public static final int INTEGER_NULL=0;
+    public static final String STRING_NULL="";
+
+
+}
